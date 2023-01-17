9379bf31234632db7411a9d44c2d1c382949b233
Anuj-Gupta4 <anujgupta.4388@gmail.com>
2023-01-17 19:59:55 +0545

    upload

---
+++ mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-uj*eg7ef8ln8r5g-3$xrjepc^q$i2gkkz!+5mn01m1cjc7(xdf"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
